d42d1ac4b70451aa877063aaea417a263cedd20d
Shan <shanasghar7@gmail.com>
2022-01-24 16:59:45 +0500

    created repository

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const heading = React.createElement("h1", null, "Hello World");
+const heading = React.createElement("h1", null, "Live Loading Integrated !");
 
 ReactDOM.render(heading, document.getElementById("root"));
--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,15 @@ const path = require('path');
 
 module.exports = {
   mode: 'production',
-  "entry": path.join(__dirname, './src/index.js'),
-  "output": {
+  entry: path.join(__dirname, './src/index.js'),
+  output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'my-first-webpack.bundle.js'
+  },
+  devServer: {
+    static: {
+        directory: path.join(__dirname, 'dist'),
+    },    
+    port: 3000
   }
 };
